f102221e98266717cc1f8147f9cd575db4c38861
cmarro3 <christophermarroquin352@gmail.com>
2020-11-25 17:04:55 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const ourArray = [];
+for(let i = 0; i < 10; i += 2){ /* The 1+= 2 is being added by 2 */
+    ourArray.push(i);
+}
+console.log(ourArray);
+
+const oddArray = [];
+for(let i = 1; i < 10; i+= 2){
+    oddArray.push(i);
+} // changing the i = 1 will print out the odd
+console.log(oddArray);
